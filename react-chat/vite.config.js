6357f1045d1f8a78705407c92a47ce5a1a587aa3
romadova irina <romadova.irina04@gmail.com>
2024-10-24 14:36:02 +0300

    деплой

--- react-chat/vite.config.js
+++ react-chat/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://romadovairina.github.io/2024-2-VK-EDU-Frontend-I-Romadova",
+  base: "2024-2-VK-EDU-Frontend-I-Romadova",
   build: {
     outDir: 'build', 
   },
